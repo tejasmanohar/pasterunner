d2b4c3328f0b4f5b457ea6decac5754311a820f0
Tejas Manohar <me@tejasmanohar.com>
2014-12-27 14:37:20 -0600

    dynamic assignment

--- app.rb
+++ app.rb
@@ -4,7 +4,8 @@
 
 
 # Gems
-require 'faraday'
+require 'nokogiri'
+require 'net/http'
 require 'json'
 require 'sinatra'
 
@@ -18,8 +19,36 @@ end
 # Helpers
 helpers do
   def eval_in(snippet)
-    res = Faraday.post('https://eval.in/', { :utf8 => 'λ', :code => snippet, :lang => 'ruby/mri-2.1' })
-    return res.body
+    ServiceURI = URI('https://eval.in/')
+    result = Net::HTTP.post_form(
+      ServiceURI,
+     "utf8" => "λ",
+     "code" => snippet,
+     "execute" => "on",
+     "lang" => "ruby/mri-2.1",
+     "input" => ""
+    )
+    if result.is_a? Net::HTTPFound
+      location = URI(result['location'])
+      location.scheme = 'https'
+      location.port = 443
+
+      body = Nokogiri(Net::HTTP.get(location))
+
+      if output_title = body.at_xpath("*//h2[text()='Program Output']")
+        output = output_title.next_element.text
+        first_line = (output.each_line.first || "").chomp
+        needs_ellipsis = output.each_line.count > 1 ||
+          first_line.length > MaxLength
+
+        "#{first_line[0, MaxLength]}#{'...' if needs_ellipsis} "\
+        "(#{location})"
+      else
+        raise FormatError, "couldn't find program output"
+      end
+    else
+      raise CommunicationError, result
+    end
   end
 end
 
@@ -40,6 +69,6 @@ post '/exec' do
   res = Faraday.get('https://api.github.com/gists/' + id)
   data = JSON.parse(res.body)
   code = data['files'].values.map { |h| h['content'] }
-
-  p eval_in(code)
+  p code[0]
+  p eval_in(code[0])
 end
